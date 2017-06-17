0b58e92d62acfe84f95ceec3d640a8d78d3688a3
wobushippmm <lhb_1566@qq.com>
2017-06-16 18:05:24 -0700

    remove

--- Main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Main.h"
-
-using namespace std;
-
-int main(int argc, char *argv[]){
-	cout << "Hello, linux" << endl;
-	return 0;
-}
--- Makefile
+++
@@ -1,8 +0,0 @@
-objects = Main.o
-server : $(objects)
-	g++ -o server $(objects)
-Main.o : Main.h
-
-clean :
-	rm -f server core $(objects)
-
